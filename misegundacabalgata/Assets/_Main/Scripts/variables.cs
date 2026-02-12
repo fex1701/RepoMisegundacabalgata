e80304c9aae21eb304a025d7f7acecb5090c5b16
fex1701 <fgarcia083@soyudemedellin.edu.co>
2026-02-12 11:39:43 -0500

    Variables
    concadenar textos

--- misegundacabalgata/Assets/_Main/Scripts/variables.cs
+++ misegundacabalgata/Assets/_Main/Scripts/variables.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
+using TMPro;
 
 public class variables : MonoBehaviour
     
 {
     //Declarar variables
 
-    string myName = "Felipe";
+    public string myName = "Felipe";
+    public int _edad = 18;
+
+    [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private TMP_Text _saludo;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      Debug.Log("Hola" + myName);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hola" + myName);
+          myName = _inputField.text;
+        _saludo.text = "Saludoss " + myName; 
     }
 }
